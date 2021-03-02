0dfea5c9a93804d864d51b659fa77a473cfe690d
amafjarkasi <amafjarkasi@gmail.com>
2021-03-02 00:59:39 +0000

    user model updated

--- src/models.py
+++ src/models.py
@@ -4,9 +4,10 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False, default=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
